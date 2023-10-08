5ac28a39f6b4af89943061d776a8b88a1b819e0a
其实雨很好 <2122840028@qq.com>
2023-10-09 07:21:25 +0800

    add 输入替换&极限雷神重新替换充能沙

---
+++ apps/InputReplace.js
@@ -0,0 +1,19 @@
+export class limit extends plugin {
+	constructor() {
+		super({
+			name: 'InputReplace',
+			dsc: '输入替换',
+			event: 'message',
+			priority: -10
+		})
+	}
+	accept(e) {
+		try {
+			if (/#?极限(面板)?/.test(e.msg)) {
+                e.msg = e.msg.replace(/#?极限(面板)?/g, '');
+                e.msg =  `#${e.msg}面板100000000`
+			}
+		} catch (err) { }
+		return false;
+	}
+}
